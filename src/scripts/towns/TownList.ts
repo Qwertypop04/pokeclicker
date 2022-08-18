a14ba194900ace1ca713d3b1362b9e47160fe401
Jaaslet <jonas_bms@hotmail.com>
2022-08-18 19:15:43 +0200

    Added Valencia Pokecenter

--- src/scripts/towns/TownList.ts
+++ src/scripts/towns/TownList.ts
@@ -2006,6 +2006,14 @@ TownList['Six Island'] = new Town(
         requirements: [new GymBadgeRequirement(BadgeEnums.Elite_HoennChampion)],
     }
 );
+TownList['Valencia Pokecenter'] = new Town(
+    'Valencia Pokecenter',
+    GameConstants.Region.hoenn,
+    [],
+    {
+        requirements: [],
+    }
+);
 
 //Hoenn Dungeons
 TownList['Petalburg Woods'] = new DungeonTown(
