1cc0c1d6855057617ec00fe526a8888726e139bc
jaaslet <jonas_bms@hotmail.com>
2022-08-19 10:12:27 +0200

    Added todo

--- src/scripts/towns/TownList.ts
+++ src/scripts/towns/TownList.ts
@@ -952,7 +952,7 @@ TownList['Kumquat Island'] = new Town(
     GameConstants.Region.kanto,
     [],
     {
-        requirements: [new GymBadgeRequirement(BadgeEnums.Elite_HoennChampion)],
+        requirements: [new GymBadgeRequirement(BadgeEnums.Elite_HoennChampion)], //TODO: unlock after defeating Four island
     }
 );
 TownList['Valencia Pokémon Center'] = new Town(
