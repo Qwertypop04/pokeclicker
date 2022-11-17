9f933e37736e55fa1feff3a503327c123eba9f9f
Qwertypop04 <63805905+Qwertypop04@users.noreply.github.com>
2022-11-17 03:50:28 +0000

    Update TemporaryBattleList.ts

--- src/scripts/temporaryBattle/TemporaryBattleList.ts
+++ src/scripts/temporaryBattle/TemporaryBattleList.ts
@@ -3512,7 +3512,7 @@ TemporaryBattleList['Ash Ketchum Alola'] = new TemporaryBattle(
     undefined,
     {
         displayName: 'Ash Ketchum',
-        returnTown: 'Seafolk Village',
+        returnTown: 'A Tree Maybe',
         imageName: 'Ash Ketchum',
         firstTimeRewardFunction: () => {
             App.game.party.gainPokemonById(25.07);
